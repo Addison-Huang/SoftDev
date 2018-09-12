713cc3244d5dff5b95512331f84f06c93ed3e8b7
Addison Huang <33487771+ahuang20stuy@users.noreply.github.com>
2018-09-12 16:32:32 -0400

    added 02_randomizer

---
+++ 02_randomizer/randomizer.py
@@ -0,0 +1,18 @@
+#Addison Huang
+#softdev1 pd6
+#K #02: NO-body expects the Spanish Inquisition!
+#2018 09 12
+
+KREWES = {'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Don\'t really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+'m': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+}
+
+import random
+def randomizer():
+    randTeam = random.randint(0,3)-1 #random number from 0 to 2
+    team = list(KREWES.keys())[randTeam] #the random team that is picked
+    randMember = random.randint(0,len(KREWES[team])) #picks a random member of the team
+    print(KREWES[team][randMember]) #prints the random member
+    
+    
