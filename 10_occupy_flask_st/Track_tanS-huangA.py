28d7595a106eba7b60a0374bf99af7ea1f5cdfe1
ahuang20stuy <ahuang20@stuy.edu>
2018-09-23 19:11:31 -0400

    10_occupy_flask_st

--- 10_occupy_flask_st/Track_tanS-huangA.py
+++ 10_occupy_flask_st/Track_tanS-huangA.py
@@ -1,30 +1,52 @@
-#Boomerang - Angela Tom & Stefan Tan
-#SoftDev1 pd6
-#K06 -- StI/O: Divine your Destiny!
-#2018-09-13
+'''
+Team Track -- Addison Huang and Stefan Tan
+Softdev1 pd6
+K10 Jinja Tuning
+2018 09 24
+'''
 
-import csv
-import random
+from flask import Flask, render_template
+import csv, random
+
+app=Flask(__name__)
 
 
-reader = csv.reader(open('occupations.csv')) #opens and reads the csv file
-next(reader) # skips the headers
-d={} #intialized the dictionary
-for row in reader: #iterates through the csv file and updates the dictionary accordingly
-    d[row[0]]=float(row[1])
-d.pop('Total') # gets rid of the last row
+@app.route("/")
+def link():
+    return "<a href = 'http://127.0.0.1:5000/occupations'> Occupations!</a>"
 
-#print(d) for testing
-
-#Returns a randomly selected occupation where the results are weighted by the percentage given.
-def selectRandOccupation(dic):
-    occupations = list(dic.keys()) #makes a list of all the occupations
-    percentages = list(dic.values()) #makes a list of all the occuaption
+@app.route("/occupations")
+def occupations():
+    reader = csv.reader(open('occupations.csv')) #opens and reads the csv file
+    next(reader) # skips the headers
+    d={} #intialized the dictionary
+    for row in reader: #iterates through the csv file and updates the dictionary accordingly
+        d[row[0]]=float(row[1])
+    d.pop('Total') # gets rid of the last row
+    occupations = list(d.keys()) #makes a list of all the occupations
+    percentages = list(d.values()) #makes a list of all the occuaption
     result = random.choices(occupations, percentages)
     #returns a list of size 1 with a random occupation choosen based on the percentages
-    print(occupations)
-    print(percentages)
-    return result.pop() #returns the only element in the list
+    return render_template("template.html",
+                           boo = result.pop(),
+                       foo="Occupations in the United States", #the title will be "Occupations in the United States"
+                       collection=d #d replaces collection in the html
+)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
 
-print('The random occupation selected is... ' + selectRandOccupation(d))
+
+
+
+
+
+
+
+
+
+
+
+
