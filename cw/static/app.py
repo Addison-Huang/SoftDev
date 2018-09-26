78a6947b114acd93b714f3968ac2db4f678387b8
ahuang20stuy <ahuang20@stuy.edu>
2018-09-26 12:13:45 -0400

    clean house

--- cw/static/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__) #create instance of class Flask
-
-@app.route("/") #assign fxn to route
-def hello_world():
-    return "<a href=/static/a.html>No hablo queso!</a>"
-
-app.debug = True
-app.run()
