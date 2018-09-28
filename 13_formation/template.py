3e1d3ecac36d4a8893336cdf193d86c482d5a7f3
ahuang20stuy <ahuang20@stuy.edu>
2018-09-27 20:19:45 -0400

    13_formation

---
+++ 13_formation/template.py
@@ -0,0 +1,32 @@
+'''
+Addison Huang 
+SoftDev1 pd6
+K13- ECHO ECHO ECHO
+2018-09-28
+'''
+
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def root():
+    print(app)
+    return render_template("template.html")
+
+@app.route("/auth")
+def authenticate():
+    print(request)
+    print(request.method)
+    print(request.args)
+    return render_template("response.html", greeting = "Why hello there",
+                           method = request.method,
+                           username = request.args["username"],
+                           args = request.args)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
+
