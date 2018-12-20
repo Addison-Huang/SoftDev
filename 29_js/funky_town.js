8569b60434b28f5480ce16a2f5546a2a68dae299
ahuang20stuy <ahuang20@stuy.edu>
2018-12-19 22:05:02 -0500

    29

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -2,20 +2,17 @@
 //Addison Huang, Michelle Tang
 //SoftDev1 pd6
 //K29 -- Sequential Progression
-//2018-12-20
+//2018-12-19
 
 var fibb = function(n){
-
     // base case 0
     if(n == 0){
 	     return 0;
     }
-
     // base case 1
     else if (n == 1){
 	     return 1;
     }
-
     // add prev two
     else{
 	     return fibb(n -1) + fibb(n -2);
@@ -23,41 +20,61 @@ var fibb = function(n){
 };
 
 var gcd = function (a,b) {
-
     // this means the previous modulo was 0, so a divided into b
     if (b == 0) {
-	     return a
+	return a;
     }
-
     // finds the smallest interval between the two numbers,
     // if that interval fully divides into b, that is the gcd
     else {
-	     return gcd(b, a % b)
+	return gcd(b, a % b);
     }
-
 };
 
-var stulist = ["james", "tyler", "lauren", "becky", "oliver", "frank"];
+var stulist = ["james", "tyler", "lauren", "becky", "taylor", "swift"];
 
 var randstu = function(){
-
     // random num in range
     var num = Math.floor(Math.random() * stulist.length);
-
     // index that
     return stulist[num];
 };
 
-var fibBut = function() {
-    console.log(fibb(8));
-    var tag = document.createElement('h4');
-    var element = document.createTextNode(fibb(8));
-    tag.appendChild(element);
-}
+// If the event is 'clicked,' follow this fib function
+var jFib = document.getElementById("fib");
+jFib.addEventListener("click", function() {
+    console.log("Finds the nth number in the fib sequence, fibb(5):");
+    var ans = fibb(5);
+    console.log(ans);
+    var para = document.createElement("h4");
+    var node = document.createTextNode("fibb(5)= " + ans);
+    para.appendChild(node);
+    var element = document.getElementById("ans");
+    element.appendChild(para);
+});
 
-var fib = document.getElementById("fib");
-fib.addEventListener('click', fibBut);
+// If the event is 'clicked,' follow this GCD function
+var jGcd = document.getElementById("gcd");
+jGcd.addEventListener("click", function() {
+    console.log("Finds the GCD, gcd(120, 75):");
+    var ans = gcd(120,75);
+    console.log(ans);
+    var para = document.createElement("h4");
+    var node = document.createTextNode("gcd(120,75)= " + ans);
+    para.appendChild(node);
+    var element = document.getElementById("ans");
+    element.appendChild(para);
+});
 
-var gcdd = document.getElementById("gcd");
-gcdd.addEventListener('click', gcdd);
-
+// If the event is 'clicked,' follow this random student function
+var jRandS = document.getElementById("rand");
+jRandS.addEventListener("click", function() {
+    console.log("Gets a random student, randstu():");
+    var ans = randstu();
+    console.log(ans);
+    var para = document.createElement("h4");
+    var node = document.createTextNode("randstu()= " + ans);
+    para.appendChild(node);
+    var element = document.getElementById("ans");
+    element.appendChild(para);
+});
